9d36a3cb4aaba39df5a93fdf33a97eec335a53cc
Olivia Mellen <omellen2269@jh-mba158-18.local>
2019-02-14 14:34:34 -0600

    created rock paper scissors, and tic tac toe layout

---
+++ rockPaperScissors/TIcTacToeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TIcTacToeViewController.swift
+//  rockPaperScissors
+//
+//  Created by Olivia Mellen on 2/12/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class TIcTacToeViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func whenPressed(_ sender: Any)
+    {
+        
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
